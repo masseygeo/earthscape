8de940f79f695be4538fed6e4d578de0f21c3d33
Matthew Massey <mamass1@g.uky.edu>
2024-10-16 15:06:04 -0400

    Updates to modeling & results

--- code/utils_training.py
+++ code/utils_training.py
@@ -45,15 +45,14 @@ def train_epoch(model, train_loader, criterion, optimizer, device):
   correct_predictions = 0
   total_batches = 0
   total_predictions = 0
-  all_targets = []
-  all_binary_predictions = []
+  # all_targets = []
+  # all_binary_predictions = []
 
   # iterate through batches
   for batch in train_loader:
 
     labels = batch.pop('label').squeeze(1).to(device)
     modalities = {modality: data.to(device) for modality, data in batch.items()}
-
     # zero the gradients
     optimizer.zero_grad()
 
@@ -68,21 +67,21 @@ def train_epoch(model, train_loader, criterion, optimizer, device):
     total_predictions += labels.numel()
     total_batches += 1
 
-    all_targets.append(labels.cpu().numpy())
+    # all_targets.append(labels.cpu().numpy())
 
     predictions = torch.sigmoid(outputs)
     binary_predictions = (predictions >= 0.5).float()
-    all_binary_predictions.append(binary_predictions.cpu().numpy())
+    # all_binary_predictions.append(binary_predictions.cpu().numpy())
     correct_predictions += (binary_predictions == labels).sum().item()
 
-  all_binary_predictions = np.concatenate(all_binary_predictions, axis=0)
-  all_targets = np.concatenate(all_targets, axis=0)
+  # all_binary_predictions = np.concatenate(all_binary_predictions, axis=0)
+  # all_targets = np.concatenate(all_targets, axis=0)
 
   epoch_loss = running_loss / total_batches
   accuracy = correct_predictions / total_predictions * 100
-  macro_f1 = f1_score(all_targets, all_binary_predictions, average='macro') 
+  # macro_f1 = f1_score(all_targets, all_binary_predictions, average='macro') 
 
-  return epoch_loss, accuracy, macro_f1
+  return epoch_loss, accuracy
 
 
 
@@ -97,8 +96,8 @@ def validate_epoch(model, val_loader, criterion, device):
   correct_predictions = 0
   total_batches = 0
   total_predictions = 0
-  all_targets = []
-  all_binary_predictions = []
+  # all_targets = []
+  # all_predictions = []
 
   with torch.no_grad():
     for batch in val_loader:
@@ -114,74 +113,101 @@ def validate_epoch(model, val_loader, criterion, device):
       total_predictions += labels.numel()
       total_batches += 1
 
-      all_targets.append(labels.cpu().numpy())
+      # all_targets.append(labels.cpu().numpy())
 
       predictions = torch.sigmoid(outputs)
+
+      # all_predictions.append(predictions.cpu().numpy())
+
       binary_predictions = (predictions >= 0.5).float()
-      all_binary_predictions.append(binary_predictions.cpu().numpy())
+      # all_binary_predictions.append(binary_predictions.cpu().numpy())
       correct_predictions += (binary_predictions == labels).sum().item()
 
-  all_binary_predictions = np.concatenate(all_binary_predictions, axis=0)
-  all_targets = np.concatenate(all_targets, axis=0)
+  # all_binary_predictions = np.concatenate(all_binary_predictions, axis=0)
+  # all_targets = np.concatenate(all_targets, axis=0)
 
   epoch_loss = running_loss / total_batches
   accuracy = correct_predictions / total_predictions * 100
-  macro_f1 = f1_score(all_targets, all_binary_predictions, average='macro') 
+  # macro_f1 = f1_score(all_targets, all_binary_predictions, average='macro') 
 
-  return epoch_loss, accuracy, macro_f1
+  return epoch_loss, accuracy
 
 
 
 
 def train_model(model, train_loader, val_loader, criterion, optimizer, device, num_epochs, output_dir):
 
-  # best_val_accuracy = 0.0
+  # out_file = open('training_output.txt', 'w')
+
   best_val_loss = float('inf')
+  best_val_accuracy = 0.0
+  # best_val_f1 = 0.0
+
   epoch_train_loss = []
   epoch_train_acc = []
-  epoch_train_macro_f1 = []
+  # epoch_train_macro_f1 = []
   epoch_val_loss = []
   epoch_val_acc = []
-  epoch_val_macro_f1 = []
+  # epoch_val_macro_f1 = []
 
   for epoch in range(num_epochs):
-    print(f"Epoch {epoch+1}/{num_epochs}")
+    epoch_str = f"Epoch {epoch+1}/{num_epochs}"
+    print(epoch_str)
+    with open(f"{output_dir}/training_output.txt", 'a') as out_file:
+      out_file.write('\n' + epoch_str + '\n')
+
     t0 = datetime.now()
     round((datetime.now()-t0).seconds / 60, 2)
 
     # training
-    train_loss, train_accuracy, train_macro_f1 = train_epoch(model, train_loader, criterion, optimizer, device)
+    train_loss, train_accuracy = train_epoch(model, train_loader, criterion, optimizer, device)
     epoch_train_loss.append(train_loss)
     epoch_train_acc.append(train_accuracy)
-    epoch_train_macro_f1.append(train_macro_f1)
+    # epoch_train_macro_f1.append(train_macro_f1)
     t1 = datetime.now()
-    print(f"TRAINING --   Time: {round((t1-t0).seconds / 60, 2)} mins.  |  Loss: {train_loss:.4f}  |  Accuracy: {train_accuracy:.2f}%  |  Macro F1: {train_macro_f1:.2f}")
+    training_str = f"TRAINING   -- Loss: {train_loss:.4f}  |  Accuracy: {train_accuracy:.2f}%  |  Time: {round((t1-t0).seconds / 60, 2)} mins."
+    print(training_str)
+    with open(f"{output_dir}/training_output.txt", 'a') as out_file:
+      out_file.write(training_str + '\n')
 
     # validation
-    val_loss, val_accuracy, val_macro_f1 = validate_epoch(model, val_loader, criterion, device)
+    val_loss, val_accuracy = validate_epoch(model, val_loader, criterion, device)
     epoch_val_loss.append(val_loss)
     epoch_val_acc.append(val_accuracy)
-    epoch_val_macro_f1.append(val_macro_f1)
+    # epoch_val_macro_f1.append(val_macro_f1)
     t2 = datetime.now()
-    print(f"VALIDATION -- Time: {round((t2-t1).seconds / 60, 2)} mins.  |  Loss: {val_loss:.4f}  |  Accuracy: {val_accuracy:.2f}%  |  Macro F1: {val_macro_f1:.2f}")
+    val_str = f"VALIDATION -- Loss: {val_loss:.4f}  |  Accuracy: {val_accuracy:.2f}%  |  Time: {round((t2-t1).seconds / 60, 2)} mins."
+    print(val_str)
+    with open(f"{output_dir}/training_output.txt", 'a') as out_file:
+      out_file.write(val_str + '\n')
 
+    # save best model...
+    if val_loss < best_val_loss:
+      best_val_loss = val_loss
+      torch.save(model.state_dict(), f"{output_dir}/best_loss.pth")
+      loss_str = f"New best model saved with loss {best_val_loss:.4f}..."
+      print(loss_str)
+      with open(f"{output_dir}/training_output.txt", 'a') as out_file:
+        out_file.write(loss_str + '\n')
+    
+    if val_accuracy > best_val_accuracy:
+      best_val_accuracy = val_accuracy
+      torch.save(model.state_dict(), f"{output_dir}/best_accuracy.pth")
+      acc_str = f"New best model saved with accuracy {best_val_accuracy:.2f}%..."
+      print(acc_str)
+      with open(f"{output_dir}/training_output.txt", 'a') as out_file:
+        out_file.write(acc_str + '\n')
+ 
     print('\n')
 
-    # save best model based on loss
-    if val_loss > best_val_loss:
-      best_val_loss = val_loss
-      torch.save(model.state_dict(), f"{output_dir}/best_model.pth")
-      print(f"New best model saved with accuracy {val_accuracy:.2f}%")
-      print('\n')
-
-  return epoch_train_loss, epoch_train_acc, epoch_train_macro_f1, epoch_val_loss, epoch_val_acc, epoch_val_macro_f1
+  return epoch_train_loss, epoch_train_acc, epoch_val_loss, epoch_val_acc
 
 
 
 
 
 
-def test_model(model, test_loader, device, output_dir):
+def test_model(model, test_loader, device):
   
   all_predictions = []
   all_targets = []
@@ -190,15 +216,16 @@ def test_model(model, test_loader, device, output_dir):
 
   with torch.no_grad():
     for batch in test_loader:
-      rgb = batch['rgb'].to(device)
-      dem = batch['dem'].to(device)
-      labels = batch['label'].squeeze(1).to(device)
+      
+      labels = batch.pop('label').squeeze(1).to(device)
+      modalities = {modality: data.to(device) for modality, data in batch.items()}
 
+      outputs = model(modalities)
+      # loss = criterion(outputs, labels)
+      predictions = torch.sigmoid(outputs)
+
+      all_predictions.append(predictions.cpu().numpy())
       all_targets.append(labels.cpu().numpy())
-
-      outputs = model(rgb, dem)
-      outputs = torch.sigmoid(outputs)
-      all_predictions.append(outputs)
   
   all_predictions = np.concatenate(all_predictions)
   all_targets = np.concatenate(all_targets)
@@ -211,12 +238,13 @@ def test_model(model, test_loader, device, output_dir):
 
 
 def calculate_label_precision_recall_f1_aucroc(predictions, targets, threshold=0.5):
+
   predictions_binary = (predictions >= threshold).astype(int)
   
-  precision = precision_score(targets, predictions_binary)
+  precision = precision_score(targets, predictions_binary, zero_division=0.0)
   recall = recall_score(targets, predictions_binary)
   f1 = f1_score(targets, predictions_binary)
-  auc_roc = roc_auc_score(targets, predictions_binary)
+  auc_roc = roc_auc_score(targets, predictions)
   
   return precision, recall, f1, auc_roc
 
@@ -225,20 +253,21 @@ def calculate_label_precision_recall_f1_aucroc(predictions, targets, threshold=0
 
 
 
-def plot_label_pr_roc_curves(target_label, predictions, targets):
+def plot_label_pr_roc_curves(predictions, targets):
   fig, ax = plt.subplots(ncols=2, figsize=(10,5))
 
   precision_vals, recall_vals, _ = precision_recall_curve(targets, predictions)
   ax[0].plot(recall_vals, precision_vals, linewidth=2)
   ax[0].set_xlabel('Recall')
   ax[0].set_ylabel('Precision')
-  ax[0].set_title(f'Precision-Recall Curve, {target_label}')
+  ax[0].set_title('Precision-Recall Curve', style='italic')
 
   fpr, tpr, _ = roc_curve(targets, predictions)
   ax[1].plot(fpr, tpr, linewidth=2)
+  ax[1].plot([0,1], [0,1], color='k', linestyle='--')
   ax[1].set_xlabel('False Positive Rate')
   ax[1].set_ylabel('True Positive Rate')
-  ax[1].set_title(f'Receiver Operating Curve, {target_label}')
+  ax[1].set_title('Receiver Operating Curve', style='italic')
 
   for axes in ax:
     axes.set_xlim(0,1)
@@ -251,9 +280,15 @@ def plot_label_pr_roc_curves(target_label, predictions, targets):
 
 
 
-def calculate_global_metrics(targets, predictions, threshold=0.5):
+def calculate_global_metrics(targets, predictions, thresholds=[0.5]):
 
-  predictions_binary = (predictions >= threshold).astype(int)
+  if len(thresholds) > 1:
+    predictions_binary = predictions
+    for idx, thresh in enumerate(thresholds):
+      predictions_binary[:, idx] = (predictions_binary[:, idx] >= thresh).astype(int)
+
+  else:
+    predictions_binary = (predictions >= thresholds[0]).astype(int)
   
   macro_precision = precision_score(targets, predictions_binary, average='macro')
   macro_recall = recall_score(targets, predictions_binary, average='macro')
@@ -269,7 +304,7 @@ def calculate_global_metrics(targets, predictions, threshold=0.5):
 
 
 
-def calculate_optimal_thresholds(model, val_loader, device, output_dir):
+def calculate_optimal_thresholds(model, val_loader, device):
   
   all_predictions = []
   all_targets = []
@@ -278,27 +313,28 @@ def calculate_optimal_thresholds(model, val_loader, device, output_dir):
 
   with torch.no_grad():
     for batch in val_loader:
-      rgb = batch['rgb'].to(device)
-      dem = batch['dem'].to(device)
-      labels = batch['label'].squeeze(1).to(device)
-
+      
+      labels = batch.pop('label').squeeze(1).to(device)
+      modalities = {modality: data.to(device) for modality, data in batch.items()}
+      outputs = model(modalities)
+      predictions = torch.sigmoid(outputs)
       all_targets.append(labels.cpu().numpy())
-
-      outputs = model(rgb, dem)
-      outputs = torch.sigmoid(outputs)
-      all_predictions.append(outputs)
+      all_predictions.append(predictions.cpu().numpy())
   
   all_predictions = np.concatenate(all_predictions)
   all_targets = np.concatenate(all_targets)
 
-  val_precision, val_recall, val_thresholds = precision_recall_curve(all_targets, all_predictions)
+  optimal_thresholds = []
 
-  val_f1 = 2 * (val_precision * val_recall) / (val_precision + val_recall)
+  for class_idx in range (all_predictions.shape[1]):
+    precision, recall, thresholds = precision_recall_curve(all_targets[:, class_idx], all_predictions[:, class_idx])
+    f1 = 2 * (precision * recall) / (precision + recall)
+    best_idx = np.argmax(f1)
+    best_threshold = thresholds[best_idx]
+    optimal_thresholds.append(best_threshold)
 
-  best_f1_idx = np.argmax(val_f1)
-  best_f1_threshold = val_f1[best_f1_idx]
+  return optimal_thresholds
 
-  return best_f1_threshold
 
 
 
