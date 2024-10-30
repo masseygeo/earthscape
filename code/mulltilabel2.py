cd24c447cd0e27a2c2a4dd506a6ef52949725dcf
Matthew Massey <mamass1@g.uky.edu>
2024-10-30 10:17:49 -0400

    Updated code, new models.

--- code/mulltilabel2.py
+++ code/mulltilabel2.py
@@ -16,7 +16,7 @@ class ResNextEncoder(nn.Module):
     def forward(self, x):
         # input shape - [batch_size, 3, 256, 256]
         output = self.encoder(x)
-        return output               # shape - [batch_size, 2048, 8, 8]
+        return output     # shape - [batch_size, 2048, 8, 8]
 
 
 
@@ -28,6 +28,7 @@ class AttentionBlock(nn.Module):
 
     def forward(self, q, kv=None):  
         # input shpae(s) - [sequence_length, batch_size, embed_dim]
+        # for ResNext5 - [64, batch_size, 2048]
         # handle self attention (one modality) or cross attention (two modalities)...
         if kv is None:
             kv = q
@@ -42,6 +43,7 @@ class AttentionBlock(nn.Module):
 class MultilabelClassification(nn.Module):
     def __init__(self, modality_configs, encoder, attention_configs=None):
         super().__init__()
+
         self.modality_convs = nn.ModuleDict()
         for modality, in_channels in modality_configs.items():
             num_channels = len(in_channels)
@@ -50,9 +52,18 @@ class MultilabelClassification(nn.Module):
         self.encoder = encoder
 
         self.attention_configs = attention_configs
-        self.attention = AttentionBlock(embed_dim=2048, num_heads=8)
 
-        self.classifier_in_chans = len(modality_configs) * 2048 * 8 * 8
+        if self.attention_configs:
+            self.attention = nn.ModuleDict()
+            for attn_name, attn_modalities in attention_configs.items():
+                self.attention[attn_name] = AttentionBlock(embed_dim=2048, num_heads=8)
+        #     self.classifier_in_chans = len(modality_configs) * 2048
+            self.classifier_in_chans = len(modality_configs) * 2048 * 8 * 8
+
+        # else:
+
+        #     self.classifier_in_chans = len(modality_configs) * 2048 * 8 * 8
+
         self.classifier = nn.Sequential(nn.Linear(self.classifier_in_chans, 1024), 
                                         nn.ReLU(), 
                                         nn.Linear(1024, 512), 
@@ -67,14 +78,14 @@ class MultilabelClassification(nn.Module):
         # encode each modality...
         for modality, data in x.items():
             preprocessed = self.modality_convs[modality](data)   # convert to 3 channels for encoder
-            encoded = self.encoder(preprocessed)
-            encoded_features[modality] = encoded
+            encoded = self.encoder(preprocessed)    # encode modalities with shared encoder [batch size, 2048, 8, 8] (ResNext)
+            encoded_features[modality] = encoded    # [batch size, 3, 256, 256]
         
         # classification WITHOUT attention...
         if not self.attention_configs:
             flattened_features = [encoded.reshape(encoded.size(0), -1) for encoded in encoded_features.values()]
             concatenated_features = torch.cat(flattened_features, dim=1)
-            output = self.classifier(concatenated_features)
+            output = self.classifier(concatenated_features)        # [batch size, 7]
             return output
 
         # clasification WITH attention...
@@ -89,23 +100,27 @@ class MultilabelClassification(nn.Module):
             
             # apply attention...
             for attn_name, attn_modalities in self.attention_configs.items():
+
+                attention_block = self.attention[attn_name]
                 
                 # self attention
                 if len(attn_modalities) == 1:
                     q = attention_ready[attn_modalities[0]]
                     kv = attention_ready[attn_modalities[0]]
-                    attention_maps[attn_name] = self.attention(q, kv)      # [sequence_length, batch_size, embed_dim]
                 
                 # cross attention
                 elif len(attn_modalities) == 2:
                     q = attention_ready[attn_modalities[0]]
                     kv = attention_ready[attn_modalities[1]]
-                    attention_maps[attn_name] = self.attention(q, kv)      # [sequence_length, batch_size, embed_dim]
+                
+                attn_output = attention_block(q, kv)
+                attn_output = attn_output.permute(1, 0, 2)    # [batch_size, sequence length, embed_dim]
+                # attn_output = attn_output.mean(dim=1)         # [batch_size, embed_dim]
+                attention_maps[attn_name] = attn_output
             
             # classification...
-            flattened_features = [attn.permute(1, 0, 2) for attn in attention_maps.values()]       # [batch_size, sequence_length, embed_dim]
-            flattened_features = [attn.reshape(attn.size(0), -1) for attn in flattened_features]   # [batch_size, sequence_length * embed_dim]
-            concatenated_features = torch.cat(flattened_features, dim=1)
+            flattened_features = [attn.reshape(attn.size(0), -1) for attn in attention_maps.values()]   # [batch_size, sequence_length * embed_dim]
+            concatenated_features = torch.cat(flattened_features, dim=1)   # [batch_size, total embed_dim]
             output = self.classifier(concatenated_features)
             return output
 
